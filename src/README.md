3ce63cd7e8a2016332a4a0797513d0e1056d3a03
WESMO <totemchik1@gmail.com>
2023-02-18 13:48:07 +0300

    first commit

---
+++ src/README.md
@@ -0,0 +1,17 @@
+## instal
+```
+npm install
+```
+run 'run lint' for lint and then development
+```
+npm run lint
+```
+run npm start for dev
+```
+npm start
+```
+
+run 'build' for production
+```
+npm run build
+```
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery'
+console.log('Hello')
+
+export const name = 'square';
+
+export function draw(ctx, length, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, length, length);
+
+  return {
+    length: length,
+    x: x,
+    y: y,
+    color: color
+  };
+}
